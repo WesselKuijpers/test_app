e66cd6a6e1d3f2100fecb544b033fc9f0bdb8371
root <root@BTO-WESSEL.localdomain>
2018-07-11 11:23:18 +0200

    created form and store action for creating a post

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,16 @@
+class PostsController < ApplicationController
+  def new
+    @post = Post.new
+  end
+
+  def create
+    # render plain: params[:post].inspect
+    @post = Post.new(post_params)
+    @post.save
+    redirect_to posts_show(@post)
+  end
+
+  def post_params
+    params.require(:post).permit(:title, :description)
+  end
+end
