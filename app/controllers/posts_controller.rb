a00bfd2dd59efaa06208584c4bd1f1bcc7c0af02
root <root@BTO-WESSEL.localdomain>
2018-07-15 21:00:47 +0200

    created edit form and infrastructure for posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -3,6 +3,20 @@ class PostsController < ApplicationController
     @post = Post.new
   end
 
+  def edit
+    @post = Post.find(params[:id])
+  end
+
+  def update
+    @post = Post.find(params[:id])
+    if @post.update(post_params)
+      flash[:notice] = "Succes!"
+      redirect_to post_path(@post)
+    else
+      render 'edit'
+    end
+  end
+
   def create
     @post = Post.new(post_params)
     if @post.save
