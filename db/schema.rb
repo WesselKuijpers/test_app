965c11dd5b512d42e60ec71f0d9091e46a01bc67
root <root@BTO-WESSEL.localdomain>
2018-08-02 11:20:04 +0200

    added password authentication backend to users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_07_19_091831) do
+ActiveRecord::Schema.define(version: 2018_08_02_091226) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
@@ -32,6 +32,7 @@ ActiveRecord::Schema.define(version: 2018_07_19_091831) do
     t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "password_digest"
   end
 
 end
